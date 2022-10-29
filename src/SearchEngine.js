c9947ac8437c67fa7a6fe1ddc0fbf9703bb49a71
MelanieMayne <103015325+MelanieMayne@users.noreply.github.com>
2022-10-28 20:02:00 -0700

    search engine partially working, will fix later

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -6,24 +6,33 @@ import "./SearchEngine.css";
 
 export default function SearchEngine(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.city);
   
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.condition.description,
+      city: response.data.city,
+      date: new Date(response.data.time * 1000),
+    });
+    }
+
   function handleSubmit(event) {
     event.preventDefault();
-  }
-  function handleChangeCity(event) {
-    console.log(event.target.value);
+    search(city);
   }
 
-  function handleResponse(response) {
-  setWeatherData({
-    ready: true,
-    temperature: response.data.temperature.current,
-    humidity: response.data.temperature.humidity,
-    wind: response.data.wind.speed,
-    description: response.data.condition.description,
-    city: response.data.city,
-    date: new Date(response.data.time * 1000),
-  });
+  function handleChangeCity(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "440461d4fbdf3d442aeb4ff32t4abo8a";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=imperial`
+    axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
@@ -40,7 +49,7 @@ export default function SearchEngine(props) {
               className="search-bar"
               onChange={handleChangeCity}
             />
-            <input type="submit" value="Search" className="search-button" />
+            <input type="submit" value="Search" className="search-button" onSubmit={handleSubmit}/>
           </form>
           <button className="geo-locale-button">
             <i className="bi bi-geo-alt-fill"></i>
@@ -51,9 +60,7 @@ export default function SearchEngine(props) {
     </div>
   );
 } else {
-  const apiKey = "440461d4fbdf3d442aeb4ff32t4abo8a";
-  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=imperial`
-  axios.get(apiUrl).then(handleResponse);
+  search();
   return <div>Loading...</div>
 }
 }
