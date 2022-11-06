e9d8291fffde77a61bb171a84436c5c18ab1a59d
MelanieMayne <103015325+MelanieMayne@users.noreply.github.com>
2022-11-06 10:45:15 -0800

    Added footer with links to my website and the projects  links on GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ export default function App() {
       >
         <SearchEngine city="Long Beach"/>
       </div>
+      <footer>
+        <p>This project was coded by <a href="https://cosmic-daffodil-494003.netlify.app/" target="_blank" rel="noreferrer">Melanie Richardson</a> and is <a href="https://github.com/MelanieMayne/My-Portfolio-Project" target="_blank" rel="noreferrer">open-sourced on GitHub</a> and <a href="https://app.netlify.com/sites/cosmic-daffodil-494003/overview" target="_blank" rel="noreferrer">hosted on Netlify</a></p>
+      </footer>
     </div>
   );
 }
